81f3f5f0ac0d214bc85de46aec52343d43c68190
mdantes20 <marco.ingrilli@gmail.com>
2025-04-24 16:25:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -67,4 +67,5 @@ def get_budget():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Render imposta PORT dinamicamente
+    app.run(host='0.0.0.0', port=port)
